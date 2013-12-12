19ff9d38414c684445cc3cc01a9bfb5172484d54
Firas Bessadok <firas.bess@gmail.com>
2013-12-12 19:52:58 +0100

    search repository

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
+import utils._
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index())
   }
-
+  
+  implicit val repositoryAsJson = Json.writes[Repository]
+  
+  def search(query: String) = Action.async {
+    Repository.search(query).map {
+      case r => Ok(Json.toJson(r))
+    }
+  }
 }
---
+++ app/utils/Repository.scala
@@ -0,0 +1,39 @@
+package utils
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+
+import play.api.libs.ws._
+
+case class Repository(fullname: String, url: String, stars: Int)
+
+object Repository {
+  
+  implicit def parseRepository: Reads[Seq[Repository]] = {
+    (__ \ "items").read(
+      seq(
+        (__ \ "full_name").read[String] and
+        (__ \ "html_url").read[String] and
+        (__ \ "stargazers_count").read[Int]
+        tupled
+      )
+    ).map(
+      _.collect {
+        case (fullname, url, stars) => Repository(fullname, url, stars)
+      }
+    )
+  }
+  
+  def search(query: String): Future[Seq[Repository]] =
+    WS.url(s"https://api.github.com/search/repositories?q=$query")
+    .get().map(
+      r => r.status match {
+        case 200 => r.json.asOpt[Seq[Repository]].getOrElse(Nil)
+        case e => sys.error(s"Bad response. Status $e")
+      }
+    )
+}
