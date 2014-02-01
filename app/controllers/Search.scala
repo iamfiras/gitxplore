c18c3e5a290f749dfd274fedf613caee40691a9e
Firas Bessadok <firas.bess@gmail.com>
2014-02-01 02:51:49 +0100

    renaming githubMsg to github

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -20,8 +20,8 @@ object Search extends Controller {
   def results(q: Option[String]) = Action.async {
     q match {
       case Some(query) if (query.trim.length > 0) =>
-        RepositoryMock.empty(query).map {
-          case r => if (r.length > 0) Ok(views.html.results(r)) else Ok(views.html.messages.githubmsg(Messages.REPO_NOT_FOUND))
+        RepositoryMock.search(query).map {
+          case r => if (r.length > 0) Ok(views.html.results(r)) else Ok(views.html.messages.github(Messages.REPO_NOT_FOUND))
         }
       case _ => scala.concurrent.Future { Ok(views.html.messages.error(Messages.EMPTY_QUERY)) }
     }
