b5b8490a7c0097bf65041adc48fe02429678f253
Firas Bessadok <firas.bess@gmail.com>
2014-02-10 11:56:05 +0100

    indentation fix

--- app/controllers/AggregateStream.scala
+++ app/controllers/AggregateStream.scala
@@ -14,21 +14,21 @@ import utils.HtmlStreamImplicits._
 
 object AggregateStream extends Controller {
 
-	def index(owner: String, repo: String) = Action.async { request =>
-		val repofullname = owner + "/" + repo
-		Future { Ok.chunked(views.stream.details.chunked(repofullname, buildHtmlStream(repofullname, request))) }
-	}
+  def index(owner: String, repo: String) = Action.async { request =>
+    val repofullname = owner + "/" + repo
+    Future { Ok.chunked(views.stream.details.chunked(repofullname, buildHtmlStream(repofullname, request))) }
+  }
 
-	def buildHtmlStream(repofullname: String, request: Request[AnyContent]): HtmlStream = {
+  def buildHtmlStream(repofullname: String, request: Request[AnyContent]): HtmlStream = {
     val commitsFuture = Commit.get(repofullname)
-		val readmeHtmlFuture = Details.getReadme(repofullname)(request).flatMap(simpleResult => PageHelper.getHtml(simpleResult))
-		val historyHtmlFuture = Details.getContributions(commitsFuture)(request).flatMap(simpleResult => PageHelper.getHtml(simpleResult))
-		val timelineHtmlFuture = Details.getTimeline(commitsFuture)(request).flatMap(simpleResult => PageHelper.getHtml(simpleResult))
+    val readmeHtmlFuture = Details.getReadme(repofullname)(request).flatMap(simpleResult => PageHelper.getHtml(simpleResult))
+    val historyHtmlFuture = Details.getContributions(commitsFuture)(request).flatMap(simpleResult => PageHelper.getHtml(simpleResult))
+    val timelineHtmlFuture = Details.getTimeline(commitsFuture)(request).flatMap(simpleResult => PageHelper.getHtml(simpleResult))
 
-		val readmeStream = PageHelper.renderStream(readmeHtmlFuture, "readmeTab")		
-		val historyStream = PageHelper.renderStream(historyHtmlFuture, "contributionsTab")
-		val timelineStream = PageHelper.renderStream(timelineHtmlFuture, "timelineScript")
+    val readmeStream = PageHelper.renderStream(readmeHtmlFuture, "readmeTab")		
+    val historyStream = PageHelper.renderStream(historyHtmlFuture, "contributionsTab")
+    val timelineStream = PageHelper.renderStream(timelineHtmlFuture, "timelineScript")
 
-		HtmlStream.interleave(readmeStream, historyStream, timelineStream)
-	}
+    HtmlStream.interleave(readmeStream, historyStream, timelineStream)
+  }
 }
